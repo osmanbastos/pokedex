c6d0f08ec45d16471ae99463230ddeb13021f372
osmanbastos <osmanbastos@hotmail.com>
2024-05-07 02:05:20 -0300

    fix: error on searchPokemon render

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,13 @@ const nextBtn = document.getElementById('nextBtn');
 let offset = 0;
 let limit = 12; // Define how many pokemons to show per page
 
+// Função para renderizar a lista de pesquisa de pokémons por nome
+function renderSearchList(pokemons) {
+    const pokemonList = document.getElementById('pokemonList');
+    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    pokemonList.innerHTML = newHtml;
+}
+
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
@@ -28,6 +35,12 @@ function changeLimit() {
     limit = parseInt(limitInput.value);
     renderPokemonList();
 }
+// Event listener para buscar ao pressionar Enter no input
+document.getElementById('limitInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        changeLimit();
+    }
+});
 
 function renderPokemonList() {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
@@ -46,5 +59,62 @@ function prevPage() {
     renderPokemonList();
 }
 
+
 // Initial render
 renderPokemonList();
+
+//searchPokemon
+// Função para converter texto para snake_case
+function toSnakeCase(text) {
+    return text.toLowerCase().replace(/\s+/g, '_');
+}
+
+// Função para buscar Pokémon por nome
+function searchPokemonByName(searchText) {
+    const searchQuery = toSnakeCase(searchText);
+
+    return pokeApi.getPokemons(0, 1118).then((pokemons) => {
+        return pokemons.filter(pokemon => {
+            const pokemonNameSnakeCase = toSnakeCase(pokemon.name);
+            return pokemonNameSnakeCase.includes(searchQuery);
+        });
+    });
+}
+
+
+
+// Função para lidar com a busca ao pressionar o botão search
+function handleSearchInput() {
+    const searchInput = document.getElementById('searchInput');
+    const searchText = searchInput.value;
+
+    if (searchText !== '') {
+        searchPokemonByName(searchText).then(results => {
+            renderPokemonList(results)
+            console.log(results)
+        });
+    } else {
+        window.onload = renderPokemonList();
+    }
+}
+
+// Função redundante para confirmar o carregamento da página
+function handleSearchInput() {
+    const searchInput = document.getElementById('searchInput');
+    const searchText = searchInput.value;
+
+    if (searchText !== '') {
+        searchPokemonByName(searchText).then(results => {
+            renderSearchList(results)
+            console.log(results)
+        });
+    } else {
+        window.onload = renderPokemonList();
+    }
+}
+// Event listener para buscar ao pressionar Enter no input
+document.getElementById('searchInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        handleSearchInput();
+    }
+});
